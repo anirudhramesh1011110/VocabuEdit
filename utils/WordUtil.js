ee2b1c91da308ba1a90ab340967d747e561aa4fc
Anirudh Ramesh <anirudhramesh1@gmail.com>
2016-09-19 14:39:03 -0400

    working on nouns

--- utils/WordUtil.js
+++ utils/WordUtil.js
@@ -1,6 +1,7 @@
 require('wndb-with-exceptions');
 const WordNet = require('node-wordnet');
 const wn = new WordNet();
+const _ = require('underscore');
 
 const WordUtil = {};
 
@@ -11,13 +12,30 @@ const WordUtil = {};
  * @param: pos Part of speech of word.
  */
 WordUtil.getSyn = function(word, pos) {
-  wn.lookup(word, (result) => {
-   for(var r in result){
-     if(result[r].pos === pos) {
-       console.log(result[r].synonyms);
+  return new Promise((resolve, reject) => {
+    var punc = '';
+    //This regex checks to see if last letter is puncuation mark.
+    if(!/[a-z 0-9]/g.test(word[word.length-1])){
+      var punc = word[word.length-1];
+      word = word.slice(0, word.length-1);
+    }
+    //Lookup word in wordnet DB
+    wn.lookup(word, (result) => {
+     var synArr = [];
+     //Make sure each synset has same lemma.
+     for(var r in result){
+       if(result[r].pos === pos && result[r].lemma === word) {
+         var syns = result[r].synonyms;
+         //No duplicate synonyms.
+         syns.forEach((s) => {
+           if(synArr.indexOf(s) === -1) synArr.push(s);
+         });
+       }
      }
-   }
-
+     //Return random synonym.
+     var i = _.random(synArr.length-1);
+     resolve(synArr[i] + punc);
+    });
   });
 
 };
--- utils/amend.js
+++ utils/amend.js
@@ -1,51 +1,72 @@
 require('wndb-with-exceptions');
-var WordNet = require('node-wordnet');
-var wn = new WordNet();
+const _ = require('underscore');
+const WordNet = require('node-wordnet');
+const AYLIENTextAPI = require('aylien_textapi');
+const Wutil = require('./WordUtil');
+const Map = require('async/map');
+
+//Create Alyien Text Api object
+var textapi = new AYLIENTextAPI({
+  application_id: process.env.AYLIEN_ID,
+  application_key: process.env.AYLIEN_KEY
+});
+
+const wn = new WordNet();
 
 var str = "You could call it tit-for-tat with a dash of Mexican drama. Mexico is to consider a proposal to revoke its treaties with the US, including the 1848 agreement that transferred half its territory to Washington if Donald Trump is elected and tries to make the US’s southern neighbour pay for a border wall. A Mexican senator is due on Tuesday to propose legislation that would empower the government to retaliate of Mr Trump inflicts expropriations or economic losses on his country to make it pay for a wall along the 2,000 mile border."
 
-var wutil = require('./WordUtil');
-wutil.getSyn('play', 'n');
+var Tagger = require("natural").BrillPOSTagger;
+var base_folder = "./node_modules/natural/lib/natural/brill_pos_tagger/data/English";
+var rules_file = base_folder + "/tr_from_posjs.txt";
+var lexicon_file = base_folder + "/lexicon_from_posjs.json";
+var default_category = 'N';
 
+/* Returns an array of each word tagged with POS.
+ *
+ * @param: text: the text to tag.
+ */
+function tagText(text) {
+  return new Promise((resolve, reject) => {
+    var tagger = new Tagger(lexicon_file, rules_file, default_category, (err) => {
+      if(err) {
+        console.log(err);
+        reject((err));
+      }else{
+        resolve(tagger.tag(str.split(' ')));
+      }
+    });
+  });
+};
 
-// var n = [];
-//
-// var Tagger = require("natural").BrillPOSTagger;
-// var base_folder = "./node_modules/natural/lib/natural/brill_pos_tagger/data/English";
-// var rules_file = base_folder + "/tr_from_posjs.txt";
-// var lexicon_file = base_folder + "/lexicon_from_posjs.json";
-// var default_category = 'N';
-//
-// var tagger = new Tagger(lexicon_file, rules_file, default_category, function(error) {
-//   if (error) {
-//     console.log(error);
-//   }
-//   else {
-//     var tagged_array = tagger.tag(str.split(' '));
-//
-//     tagged_array.forEach(function(wordArr){
-//       if(wordArr[1] === 'N' || wordArr[1] === 'NN'){
-//         wn.lookup(wordArr[0], function(results) {
-//             var synArr = [];
-//           	results.forEach(function(result) {
-//               //TODO: NEED TO FIGURE WHICH SYNSET IS BEST TO USE.
-//               	if(result.pos === 'n'){
-//                   result.synonyms.forEach(function(s){
-//                     synArr.push(s);
-//                   });
-//
-//         				}
-//             });
-//             if(synArr[1] != undefined){
-//               n.push(synArr[1]);
-//             }else{
-//               n.push(wordArr[0]);
-//             }
-//
-//         });
-//         console.log(n);
-//       }
-//
-//     });
-//   }
-// });
+/* Returns a new array of synonyms of original words (nouns for now).
+ *
+ * @param result: tagged array.
+ */
+function mapArr(result) {
+  return new Promise((resolve, reject) => {
+    var arr = _.map(result, (i) => {
+      //Only does nouns.
+      if(i[1] === 'N' || i[1] === 'NN') {
+        Wutil.getSyn(i[0], 'n').then((res) =>{
+          //console.log(i[0] + ' --> ' + res);
+          if(res === 'undefined'){
+            //console.log(i[0]);
+            return i[0];
+          }else{
+            //console.log(res);
+            return res;
+          }
+        });
+      }else{
+        return i[0];
+      }
+    });
+    resolve(arr);
+  });
+};
+
+tagText(str).then((result) => {
+  mapArr(result).then((res) => {
+    console.log(res);
+  });
+});
